41634d08270ce2c6e9edb196f9357e02f57e1216
URIAN VIERA <urian1213viera@gmail.com>
2024-02-05 13:43:41 -0500

    agregando readme

--- src/components/ApiSimpsons.jsx
+++ src/components/ApiSimpsons.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "../assets/css/home.css";
-import miImagen from '../assets/imgs/los_simpsons.gif';
+import miImagen from "../assets/imgs/los_simpsons.gif";
 
-function ApiSimpsons() {
+const ApiSimpsons = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
 
@@ -17,7 +17,7 @@ function ApiSimpsons() {
         console.log(response.data);
         setData(response.data);
       } catch (error) {
-        console.log('Erro en la carga')
+        console.log("Erro en la carga");
       } finally {
         setLoading(false);
       }
@@ -30,13 +30,12 @@ function ApiSimpsons() {
     return <div>Cargando espere...</div>;
   }
 
-
   return (
     <>
       <div className="row justify-content-md-center">
         <h1>
           Personajes de los Simpsons <br />
-          <img src={miImagen} className="rounded logo_Simpsons" alt="Los Simpsons" />
+          <img src={miImagen} className="img-fluid" alt="Los Simpsons" />
           <hr />
         </h1>
       </div>
@@ -44,7 +43,7 @@ function ApiSimpsons() {
         {data.map((item, index) => (
           <div key={index} className="col-md-3 card-4">
             <div className="card-content mb-5">
-              <img src={item.image} className="img_simpsons mb-3"/>
+              <img src={item.image} className="img_simpsons mb-3" />
             </div>
             <div className="card-footer mt-5">
               <p>{item.character}</p>
@@ -54,6 +53,6 @@ function ApiSimpsons() {
       </div>
     </>
   );
-}
+};
 
 export default ApiSimpsons;
